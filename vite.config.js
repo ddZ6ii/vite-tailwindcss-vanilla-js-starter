ff1ed5bbf7e0f4f5c52b6965e81b2ace234a304e
Denis Dezest <denis.dezest@gmail.com>
2024-04-18 18:32:40 +0200

    🔧 Update vite config file: add aliases for import and use absolute path files

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Define chrome as default browser for the dev server.
 const opsys = process.platform;
@@ -9,12 +13,18 @@ if (opsys === 'win32') process.env.BROWSER = 'chrome';
 if (opsys === 'darwin') process.env.BROWSER = '/Applications/Google Chrome.app';
 
 export default defineConfig({
-  root: './src',
-  publicDir: '../public',
+  root: resolve(__dirname, './src'),
+  publicDir: resolve(__dirname, './public'),
   build: {
     emptyOutDir: true,
-    outDir: '../dist',
+    outDir: resolve(__dirname, './dist'),
     sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@assets': resolve(__dirname, './assets'),
+      '@js': resolve(__dirname, './src/js'),
+    },
   },
   server: {
     port: 3000,
